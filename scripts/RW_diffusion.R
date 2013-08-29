b990f820f16276d2271e30cb34076cd5ca89b0f7
dgravel <dominique_gravel@uqar.ca>
2013-08-29 13:55:25 -0400

    Corrections #2

--- scripts/RW_diffusion.R
+++ scripts/RW_diffusion.R
@@ -1,55 +1,47 @@
 rm(list=ls())
 library(deSolve)
-
-# Parameters
-D = 0.1    
-r = 1
-K = 0.7
-a = 1
-b = 10
-d = 0.1
-			
+	
+# SPATIAL PARAMATERS
 # Cell size
 delx = 1
 
 # Number of cells
-numboxes = 100
+numboxes = 10
 	
 # Sequence
 Distance = seq(from=0.5, by=delx, length.out = numboxes)
 
+# DEFINE THE MODEL
 model = function(Time, State, Pars) {
 with(as.list(c(State,Pars)), {
 	dR 		= r*R*(1-R/K) - a*R*C/(1+b*R)	
 	deltax 	= c(0.5,rep(1,numboxes-1),0.5)    
-	Flux    	= -D*diff(c(0,C,0))/deltax
+	Flux    = -D*diff(c(0,C,0))/deltax
 	dC    	= -diff(Flux)/delx + a*R*C/(1+b*R) - d*C
 	list(c(dR,dC))
 	})
 }
 
-R = rep(K,times = numboxes)       
+# DEFINE ALL INITIAL CONDITIONS
+R = rep(1,times = numboxes)       
 C = rep(0.01,times = numboxes)
-C[50:51] = 0.1
+C[numboxes/2] = 0.1
+state = c(R = R, C = C)  
 
-state = c(R = R, C = C)          
+# PARAMETERS 
+pars = c(r = 1, K = 0.7, a = 1, b = 10, d = 0.1, D = 0.)	
+times = seq(0,1000,by=0.1)             
 
-times = seq(0,10,by=0.01)             
+# RUN THE SIMULATION
+out = ode(y=state,times=times,func=model,parms=pars)  
 
-out = ode(y=state,times=times,func=model,parms=0)  
+# PLOT THE RESULTS
+#par(oma=c(0,0,3,0))   
+#color = topo.colors
+#filled.contour(x=times,y=Distance,z = out[,(numboxes+2):ncol(out)],color= color, xlab="time", ylab= "Distance",main="Density")
 
-DENSITY   <- out[,2:(numboxes+1)]
+plot(x = times, y = out[,(numboxes + numboxes/2)], type = "l")
 
-# set margins
-par(mfrow=c(1,1))
-par(oma=c(0,0,3,0))   
-
-# set colors
-color = topo.colors
-
-# make the plot
-filled.contour(x=times,y=Distance,DENSITY,color= color,
-xlab="time", ylab= "Distance",main="Density")
-
+#plot(x = times, y = out[,(numboxes/2)], type = "l")
 
 
