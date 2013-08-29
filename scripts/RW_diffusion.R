f562694b47250fac8f47a15618db917521465f9a
dgravel <dominique_gravel@uqar.ca>
2013-08-29 13:42:07 -0400

    Corrections on the script RM-Diffusion

--- scripts/RW_diffusion.R
+++ scripts/RW_diffusion.R
@@ -2,12 +2,12 @@ rm(list=ls())
 library(deSolve)
 
 # Parameters
-D = 0.3    
+D = 0.1    
 r = 1
-K = 1
+K = 0.7
 a = 1
 b = 10
-d = 0.2
+d = 0.1
 			
 # Cell size
 delx = 1
@@ -18,24 +18,25 @@ numboxes = 100
 # Sequence
 Distance = seq(from=0.5, by=delx, length.out = numboxes)
 
-model <-function(t,N,parameters)	{
+model = function(Time, State, Pars) {
+with(as.list(c(State,Pars)), {
 	dR 		= r*R*(1-R/K) - a*R*C/(1+b*R)	
 	deltax 	= c(0.5,rep(1,numboxes-1),0.5)    
 	Flux    	= -D*diff(c(0,C,0))/deltax
-	dC    	= -diff(Flux)/delx  + a*R*C/(1+b*R)
+	dC    	= -diff(Flux)/delx + a*R*C/(1+b*R) - d*C
 	list(c(dR,dC))
-}  
+	})
+}
 
 R = rep(K,times = numboxes)       
-C = rep(0.1,times = numboxes)
-C[50:51] = 1
+C = rep(0.01,times = numboxes)
+C[50:51] = 0.1
 
 state = c(R = R, C = C)          
 
-times = seq(0,100,by=0.1)             
+times = seq(0,10,by=0.01)             
 
 out = ode(y=state,times=times,func=model,parms=0)  
-
 
 DENSITY   <- out[,2:(numboxes+1)]
 
